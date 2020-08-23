5443914310db607860da0d74c6b77f157f5e631a
조경진 <kj_cho@idealidea.co.kr>
2020-08-23 14:42:35 +0900

    주석 삭제

--- app/src/main/java/kr/co/tjoeun/loginlogictest_20200823/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/loginlogictest_20200823/MainActivity.kt
@@ -11,20 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//            로그인 버튼이 눌렸을때
         loginBtn.setOnClickListener {
-
-//            아이디 / 비번에 뭐라고 적혀있는지 각각 변수로 저장
             val inputId = idEdt.text.toString()
             val inputPw = pwEdt.text.toString()
 
-//            아이디 : admin@test.com AND 비번 : qwer 질문 (검사)
             if (inputId == "admin@test.com" && inputPw == "qwer") {
-//                아이디 / 비번 둘다 정확히 입력한 경우
                 Toast.makeText(this, "관리자 로그인 입니다.", Toast.LENGTH_SHORT).show()
             }
             else {
-//                둘중 하나라도 틀리면 들어오는 분기
                 Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
             }
 
